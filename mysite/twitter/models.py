880f998da3708470f8fec2e2dd1b1c529ec057c0
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-06-26 06:08:31 -0400

    refactor: database changes

--- mysite/twitter/models.py
+++ mysite/twitter/models.py
@@ -2,17 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Post(models.Model):
-    post_text = models.CharField(max_length=255)
-    created_on = models.DateTimeField(default=timezone.now())
 
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=150)
-    second_name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
     is_active = models.BooleanField()
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+    password = models.CharField(max_length=150)
     
     def __str__(self):
         return self.username
+    
+class Post(models.Model):
+    post_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.post_text
