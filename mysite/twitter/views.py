79065eb002a4009ae094a0658481008bb8b67fc6
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-06-26 06:09:08 -0400

    feat: add post_home view

--- mysite/twitter/views.py
+++ mysite/twitter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
+
+def post_home(request):
+    post_list = Post.objects.order_by('-created_on')
+    context = {'post_list': post_list}
+    
+    return render(request, 'user_create.html', context)
