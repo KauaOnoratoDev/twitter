b1082231001ba08459cfa8499124fda3840b2264
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-06-28 07:31:44 -0400

    feat: add CRUD urls

--- mysite/twitter/urls.py
+++ mysite/twitter/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.feed, name='feed'),
     path('post/<int:id>', views.post_detail, name='post_detail'),
+    path('newpost/', views.new_post, name='new_post'),
+    path('post/deletepost/<int:id>', views.delete_post, name='delete_post'),
 ]
